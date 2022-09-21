9149191185fb398ae435862d259bb341d509875c
yousafsabir <yousafsabir430516@gmail.com>
2022-09-21 20:02:48 +0500

    favourite button added

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdOutlineStar } from "react-icons/md";
 import { Link, useNavigate } from "react-router-dom";
 
 const PropertyCard = ({
@@ -14,6 +15,8 @@ const PropertyCard = ({
 }) => {
     const navigate = useNavigate();
 
+    const favourite = false;
+
     // Redirect to property detail
     const redirect = (id) => {
         navigate("/propertydetail/" + id);
@@ -24,6 +27,23 @@ const PropertyCard = ({
                 <img src={img} alt="" className="img-a" />
             </div>
             <div className="card-overlay">
+                {/* Favourite button */}
+                <button
+                    className={
+                        favourite
+                            ? "card-fovourite-btn active"
+                            : "card-fovourite-btn"
+                    }
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        className={favourite ? "active" : ""}
+                    >
+                        <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+                    </svg>
+                </button>
+
                 <div className="card-overlay-a-content">
                     <div className="card-header-a">
                         <h2 className="card-title-a">
